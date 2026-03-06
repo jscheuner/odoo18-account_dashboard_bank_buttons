15ed018950e98154b245825f2deb5017e87c76e2
jscheuner <joel.scheuner@jscheunersarl.ch>
2026-03-06 15:03:30 +0100

    Initial commit - Odoo bank dashboard buttons

---
+++ README.md
@@ -0,0 +1,18 @@
+# Account Dashboard Bank Buttons
+
+Module Odoo 18 Community ajoutant deux boutons sur les journaux bancaires du dashboard :
+
+- Relevés bancaires
+- Import CAMT
+
+## Dépendances
+
+- account
+- account_statement_import_file (OCA)
+
+## Installation
+
+1. Copier le module dans votre dossier custom addons
+2. Redémarrer Odoo
+3. Mettre à jour la liste des applications
+4. Installer "Account Dashboard Bank Buttons"
---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Account Dashboard Bank Buttons",
+    "version": "18.0.1.0.0",
+    "summary": "Add Bank Statement and CAMT import buttons on bank journals dashboard",
+    "category": "Accounting",
+    "author": "Custom",
+    "license": "LGPL-3",
+    "depends": [
+        "account",
+        "account_statement_import_file",
+    ],
+    "data": [
+        "views/account_journal_dashboard.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
